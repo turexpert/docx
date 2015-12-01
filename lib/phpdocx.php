97ea8ddbdee183afaf2887a18a6571b661cd44da
turexpert <nikitorovich@gmail.com>
2015-12-01 16:37:26 +0300

    Update phpdocx.php

--- lib/phpdocx.php
+++ lib/phpdocx.php
@@ -1,14 +1,9 @@
 <?php
 
+namespace docx;
 /**************************************
 /
-/  *** PHP Library PHPDocx ***
-/  Version: 0.9.2
-/  Author: Alexey Kichaev
-/  Home page: http://webli.ru/phpdocx/
-/  License: MIT or GPL
-/  GITHub: https://github.com/alkich/PHPDocx
-/  Date: 14/03/2012
+
 /
 /***************************************/
 
